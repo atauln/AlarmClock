768f07bcf5f820ba6560808ca0c734e5851f07bf
Ataul <ataulnoor75@gmail.com>
2018-11-24 18:15:20 -0500

    gfdkf

--- AlarmClock1/Form1.Designer.vb
+++ AlarmClock1/Form1.Designer.vb
@@ -22,29 +22,102 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.components = New System.ComponentModel.Container()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.lblTimeDisplay = New System.Windows.Forms.Label()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.TimeDetector = New System.Windows.Forms.Timer(Me.components)
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'TabControl1
         '
-        Me.Button1.Location = New System.Drawing.Point(629, 61)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(54, 74)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(800, 450)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.lblTimeDisplay)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(792, 421)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Alarms"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'lblTimeDisplay
+        '
+        Me.lblTimeDisplay.Location = New System.Drawing.Point(646, 396)
+        Me.lblTimeDisplay.Name = "lblTimeDisplay"
+        Me.lblTimeDisplay.Size = New System.Drawing.Size(143, 22)
+        Me.lblTimeDisplay.TabIndex = 0
+        Me.lblTimeDisplay.Text = "Time updating..."
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(792, 421)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Alarm Type"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(792, 421)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Settings"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Size = New System.Drawing.Size(792, 421)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Credits"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'TimeDetector
+        '
+        Me.TimeDetector.Enabled = True
+        Me.TimeDetector.Interval = 1000
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents TimeDetector As Timer
+    Friend WithEvents lblTimeDisplay As Label
 End Class
--- AlarmClock1/Form1.vb
+++ AlarmClock1/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Dim CurrentTime = Now
+    Private Sub TimeDetector_Tick(sender As Object, e As EventArgs) Handles TimeDetector.Tick
+        CurrentTime = Now
+        lblTimeDisplay.Text = CurrentTime
+    End Sub
 End Class
