e2134796df9408f3c831c69fea4de0ae903a269b
Ataul <ataulnoor75@gmail.com>
2018-11-25 10:43:53 -0500

    push

--- AlarmClock1/Form1.Designer.vb
+++ AlarmClock1/Form1.Designer.vb
@@ -25,6 +25,11 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.udMINAlarm1 = New System.Windows.Forms.NumericUpDown()
+        Me.udHRAlarm1 = New System.Windows.Forms.NumericUpDown()
+        Me.dtpAlarm1 = New System.Windows.Forms.DateTimePicker()
+        Me.txtAlarm1Name = New System.Windows.Forms.TextBox()
         Me.lblTimeDisplay = New System.Windows.Forms.Label()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
@@ -32,6 +37,9 @@ Partial Class Form1
         Me.TimeDetector = New System.Windows.Forms.Timer(Me.components)
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.udMINAlarm1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.udHRAlarm1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -49,6 +57,7 @@ Partial Class Form1
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
         Me.TabPage1.Controls.Add(Me.lblTimeDisplay)
         Me.TabPage1.Location = New System.Drawing.Point(4, 25)
         Me.TabPage1.Name = "TabPage1"
@@ -58,10 +67,54 @@ Partial Class Form1
         Me.TabPage1.Text = "Alarms"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.udMINAlarm1)
+        Me.GroupBox1.Controls.Add(Me.udHRAlarm1)
+        Me.GroupBox1.Controls.Add(Me.dtpAlarm1)
+        Me.GroupBox1.Controls.Add(Me.txtAlarm1Name)
+        Me.GroupBox1.Location = New System.Drawing.Point(11, 11)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(157, 385)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "AlarmSet1"
+        '
+        'udMINAlarm1
+        '
+        Me.udMINAlarm1.Location = New System.Drawing.Point(20, 133)
+        Me.udMINAlarm1.Name = "udMINAlarm1"
+        Me.udMINAlarm1.Size = New System.Drawing.Size(120, 22)
+        Me.udMINAlarm1.TabIndex = 2
+        '
+        'udHRAlarm1
+        '
+        Me.udHRAlarm1.Location = New System.Drawing.Point(20, 105)
+        Me.udHRAlarm1.Name = "udHRAlarm1"
+        Me.udHRAlarm1.Size = New System.Drawing.Size(120, 22)
+        Me.udHRAlarm1.TabIndex = 3
+        '
+        'dtpAlarm1
+        '
+        Me.dtpAlarm1.CustomFormat = "'MM'/'dd'/'yyyy'"
+        Me.dtpAlarm1.Location = New System.Drawing.Point(20, 77)
+        Me.dtpAlarm1.Name = "dtpAlarm1"
+        Me.dtpAlarm1.Size = New System.Drawing.Size(122, 22)
+        Me.dtpAlarm1.TabIndex = 1
+        '
+        'txtAlarm1Name
+        '
+        Me.txtAlarm1Name.Location = New System.Drawing.Point(20, 21)
+        Me.txtAlarm1Name.Name = "txtAlarm1Name"
+        Me.txtAlarm1Name.Size = New System.Drawing.Size(122, 22)
+        Me.txtAlarm1Name.TabIndex = 0
+        Me.txtAlarm1Name.Text = "Alarm Name"
+        '
         'lblTimeDisplay
         '
         Me.lblTimeDisplay.Location = New System.Drawing.Point(646, 396)
         Me.lblTimeDisplay.Name = "lblTimeDisplay"
+        Me.lblTimeDisplay.RightToLeft = System.Windows.Forms.RightToLeft.Yes
         Me.lblTimeDisplay.Size = New System.Drawing.Size(143, 22)
         Me.lblTimeDisplay.TabIndex = 0
         Me.lblTimeDisplay.Text = "Time updating..."
@@ -109,6 +162,10 @@ Partial Class Form1
         Me.Text = "Form1"
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.udMINAlarm1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.udHRAlarm1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -120,4 +177,9 @@ Partial Class Form1
     Friend WithEvents TabPage4 As TabPage
     Friend WithEvents TimeDetector As Timer
     Friend WithEvents lblTimeDisplay As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtAlarm1Name As TextBox
+    Friend WithEvents dtpAlarm1 As DateTimePicker
+    Friend WithEvents udMINAlarm1 As NumericUpDown
+    Friend WithEvents udHRAlarm1 As NumericUpDown
 End Class
--- AlarmClock1/Form1.vb
+++ AlarmClock1/Form1.vb
@@ -1,7 +1,25 @@
 ﻿Public Class Form1
-    Dim CurrentTime = Now
+    Dim CurrentTime = TimeString
     Private Sub TimeDetector_Tick(sender As Object, e As EventArgs) Handles TimeDetector.Tick
-        CurrentTime = Now
+        CurrentTime = TimeString
         lblTimeDisplay.Text = CurrentTime
+        Dim NowTime = Now.ToString
+        Dim DateValueAlarm1 = FormatDateTime(dtpAlarm1.Value.ToString, DateFormat.ShortDate)
+        If NowTime.Substring(0, 2) = DateValueAlarm1.Substring(0, 2) And NowTime.Substring(3, 2) = DateValueAlarm1.Substring(3, 2) And NowTime.Substring(6, 2) = DateValueAlarm1.Substring(6, 2) And NowTime.Substring(10, 2) = udHRAlarm1.Value And NowTime.Substring(14, 2) = udMINAlarm1.Value Then
+            MsgBox("Your alarm, " + txtAlarm1Name.Text + "has been activated", , txtAlarm1Name.Text)
+        End If
+    End Sub
+
+    Private Sub txtAlarm1Name_TextChanged(sender As Object, e As EventArgs) Handles txtAlarm1Name.TextChanged
+        Dim Alarm1Name = txtAlarm1Name.Text
+    End Sub
+
+    Private Sub dtpAlarm1_ValueChanged(sender As Object, e As EventArgs) Handles dtpAlarm1.ValueChanged
+        Dim dtpDateTime1 = FormatDateTime(dtpAlarm1.Value.ToString, DateFormat.ShortDate)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        udHRAlarm1.Value = TimeString.Substring(0, 2)
+        udMINAlarm1.Value = TimeString.Substring(3, 2)
     End Sub
 End Class
